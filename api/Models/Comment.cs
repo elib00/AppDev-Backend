edc4cdbfe47ba5d0349b7088374c74831be60a10
elib00 <joshuanapinas19@gmail.com>
2024-09-16 21:54:19 +0800

    [Joshua] Learning the basics of dotnet core

---
+++ api/Models/Comment.cs
@@ -0,0 +1,12 @@
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public String Title { get; set; } = String.Empty;
+        public String Content { get; set; } = String.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
