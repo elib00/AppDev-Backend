f20ad0e58faf3780f7915642ea39a7ee1c93edc9
elib00 <joshuanapinas19@gmail.com>
2024-09-16 22:16:10 +0800

    [Joshua] Added connection string

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,9 +1,15 @@
+using api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
 
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
     }
 }
